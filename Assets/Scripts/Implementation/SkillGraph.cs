08c0fa311f52559d3fb000ef6485a43224c57b94
Rumtery <artromskiy@gmail.com>
2023-11-02 22:16:05 +0300

    Added IsReachable method to SkillGraph, added more comments

--- Assets/Scripts/Implementation/SkillGraph.cs
+++ Assets/Scripts/Implementation/SkillGraph.cs
@@ -17,13 +17,77 @@ public class SkillGraph<T> : ISerializationCallbackReceiver
     private T _serializedRoot;
     private readonly List<KeydList> _serializedData;
 
+    /// <summary>
+    /// Checks if element is still reachable from root
+    /// even if some vertices will be skipped during search
+    /// </summary>
+    /// <param name="search"></param>
+    /// <param name="skip"></param>
+    /// <returns></returns>
+    public bool IsReachable(T search, Predicate<T> skip)
+    {
+        if (skip(search))
+            return false;
+
+        if (!_data.ContainsKey(search))
+            return false;
+
+        if (search.Equals(_root))
+            return true;
+
+        Queue<T> searchQueue = new();
+        HashSet<T> checkedVertices = new();
+
+        searchQueue.Enqueue(_root);
+        checkedVertices.Add(_root);
+
+        foreach (var vertex in _data)
+            if (skip(vertex.Key))
+                checkedVertices.Add(vertex.Key);
+
+        while (searchQueue.Count != 0)
+        {
+            var current = searchQueue.Dequeue();
+            foreach (var item in _data[current])
+            {
+                if (!checkedVertices.Contains(item))
+                {
+                    if (item.Equals(search))
+                        return true;
+
+                    searchQueue.Enqueue(item);
+                    checkedVertices.Add(item);
+                }
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Used for serialization of _data
+    /// and deserialization from _serializedData.
+    /// This is draft solution,
+    /// in real app serialization library should be used
+    /// </summary>
+    [Serializable]
+    private struct KeydList
+    {
+        public T key;
+        public List<T> values;
+
+        public KeydList(T key, List<T> values)
+        {
+            this.key = key;
+            this.values = values;
+        }
+    }
     public void OnAfterDeserialize()
     {
         _root = _serializedRoot;
         _data = new();
         foreach (var item in _serializedData)
             _data.Add(item.key, new(item.values));
-        if(!IsValid(out var message))
+        if (!IsValid(out var message))
             throw new SerializationException(message);
     }
 
@@ -37,18 +101,6 @@ public class SkillGraph<T> : ISerializationCallbackReceiver
             throw new SerializationException(message);
     }
 
-    [Serializable]
-    private struct KeydList
-    {
-        public T key;
-        public List<T> values;
-
-        public KeydList(T key, List<T> values)
-        {
-            this.key = key;
-            this.values = values;
-        }
-    }
 
     /// <summary>
     /// Checks the correctness of the graph.
@@ -61,14 +113,14 @@ public class SkillGraph<T> : ISerializationCallbackReceiver
     private bool IsValid(out string message)
     {
         message = string.Empty;
-        if(!_data.ContainsKey(_root))
+        if (!_data.ContainsKey(_root))
         {
             message = "Graph does not contain root";
             return false;
         }
         foreach (var relations in _data)
         {
-            if(relations.Value.Contains(relations.Key))
+            if (relations.Value.Contains(relations.Key))
             {
                 message = "Vertex can not be self referenced";
                 return false;
@@ -88,7 +140,7 @@ public class SkillGraph<T> : ISerializationCallbackReceiver
         notCheckedVertices.Remove(_root);
         Queue<T> searchQueue = new();
         searchQueue.Enqueue(_root);
-        while(searchQueue.Count != 0)
+        while (searchQueue.Count != 0)
         {
             var current = searchQueue.Dequeue();
             foreach (var item in _data[current])
@@ -100,7 +152,7 @@ public class SkillGraph<T> : ISerializationCallbackReceiver
                 }
             }
         }
-        if(notCheckedVertices.Count > 0)
+        if (notCheckedVertices.Count > 0)
         {
             message = "Not all vertices reachable from root";
             return false;
